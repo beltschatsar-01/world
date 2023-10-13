baa462540009b0bd5adbac71434dc78d5a42f4d4
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-10-13 21:24:27 +0300

    world 5

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose, { Mongoose } from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('successfully connected');
+})
 
 const app = express();
 
