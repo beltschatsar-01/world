8bc246daa2f275bbd95659ef189182778d842a87
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-10-22 23:05:03 +0300

    world 12

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,114 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Start loading state
+      dispatch(signInStart(true));
+
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        // Handle sign-in failure
+        dispatch(signInFailure(data.message));
+      } else {
+        // Handle sign-in success
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      // Handle network errors, failed to fetch, etc.
+      dispatch(signInFailure(error.message));
+    } finally {
+      // Stop loading state
+      dispatch(signInStart(false));
+    }
+  };
+
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to={'/sign-up'} className='text-blue-700'>
+          Sign Up
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
+};
+
+export default SignIn;
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice'
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({});
+ const {loading,error} = useSelector((state)=>state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -27,19 +130,20 @@ const SignIn = () => {
       const data = await res.json();
 
       if (data.success === false) {
-        setError(data.message);
+        dispatch(signInFailure(data.message))
         return;
       }
 
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/')
       
     } catch (error) {
-      setError('An error occurred during sign up. Please try again.');
+      dispatch(signInFailure(error.message));
     } finally {
       setLoading(false);
     }
   };
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -87,5 +191,5 @@ const SignIn = () => {
 export default SignIn;
 
 
-
+*/
 
