89218e897ba52ffd8e07bce087f99daa56c4fd7a
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-10-24 05:12:33 +0300

    world 13

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.user);
+  const {loading,error} = useSelector((state)=>state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -15,14 +16,13 @@ const SignIn = () => {
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     try {
       // Start loading state
-      dispatch(signInStart(true));
-
+      dispatch(signInStart());
+  
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -30,155 +30,57 @@ const SignIn = () => {
         },
         body: JSON.stringify(formData),
       });
-
+  
+  
       const data = await res.json();
-
+  
+      console.log(data); // Log the data received from the server
+  
       if (data.success === false) {
         // Handle sign-in failure
         dispatch(signInFailure(data.message));
-      } else {
+      }
         // Handle sign-in success
+       
         dispatch(signInSuccess(data));
         navigate('/');
-      }
-    } catch (error) {
-      // Handle network errors, failed to fetch, etc.
-      dispatch(signInFailure(error.message));
-    } finally {
-      // Stop loading state
-      dispatch(signInStart(false));
-    }
-  };
-
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          type='email'
-          placeholder='email'
-          className='border p-3 rounded-lg'
-          id='email'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='password'
-          className='border p-3 rounded-lg'
-          id='password'
-          onChange={handleChange}
-        />
-        <button
-          disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '
-        >
-          {loading ? 'Loading...' : 'Sign In'}
-        </button>
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont Have an account?</p>
-        <Link to={'/sign-up'} className='text-blue-700'>
-          Sign Up
-        </Link>
-      </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
-    </div>
-  );
-};
-
-export default SignIn;
-
-
-
-
-
-
-
-
-
-
-
-
-/*import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice'
-
-const SignIn = () => {
-  const [formData, setFormData] = useState({});
- const {loading,error} = useSelector((state)=>state.user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
-  };
-
-  const fetchData = async () => {
-    try {
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      const data = await res.json();
-
-      if (data.success === false) {
-        dispatch(signInFailure(data.message))
-        return;
-      }
-
-      dispatch(signInSuccess(data));
-      navigate('/')
       
     } catch (error) {
+      // Handle network errors
       dispatch(signInFailure(error.message));
-    } finally {
-      setLoading(false);
-    }
+    } 
   };
   
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    await fetchData();
-  };
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-       
         <input
           type='email'
-          placeholder='email'
+          placeholder='Email'
           className='border p-3 rounded-lg'
           id='email'
+          value={formData.email}
           onChange={handleChange}
         />
         <input
           type='password'
-          placeholder='password'
+          placeholder='Password'
           className='border p-3 rounded-lg'
           id='password'
+          value={formData.password}
           onChange={handleChange}
         />
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Dont Have an account?</p>
+        <p>Don't have an account?</p>
         <Link to={'/sign-up'} className='text-blue-700'>
           Sign Up
         </Link>
@@ -189,7 +91,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-
-*/
-
