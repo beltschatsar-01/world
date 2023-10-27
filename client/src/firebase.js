f41f1ff3337f8d997121ca975c112bebebe911b7
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-10-27 20:24:42 +0300

    www 2

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBAS_API_KEY ,
-  authDomain: "world-estate-9e0af.firebaseapp.com",
-  projectId: "world-estate-9e0af",
-  storageBucket: "world-estate-9e0af.appspot.com",
-  messagingSenderId: "428898071706",
-  appId: "1:428898071706:web:e2b9d1e58a200fa4dc5b23"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "world-estate-51348.firebaseapp.com",
+  projectId: "world-estate-51348",
+  storageBucket: "world-estate-51348.appspot.com",
+  messagingSenderId: "409716054767",
+  appId: "1:409716054767:web:6b50df082110dd1c4e591a"
 };
 
 // Initialize Firebase
