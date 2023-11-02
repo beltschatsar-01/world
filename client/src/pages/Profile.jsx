5eedd678befbde180bb0a9b0be911e76475d6d29
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2023-11-02 21:12:34 +0300

    sign up

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,10 +11,12 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
- // deleteUserFailure,
-  //deleteUserStart,
-  //deleteUserSuccess,
-  //signOutUserStart,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -99,13 +101,36 @@ const handleSubmit = async (e) => {
     dispatch(updateUserFailure(error.message));
   }
 };
-  const handleDeleteUser = async () => {
-    try {
-      
-    } catch (error) {
-      
+const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
     }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
   }
+};
+const handleSignOut = async () => {
+  try {
+    dispatch(signOutUserStart());
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(data.message));
+  }
+};
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -173,9 +198,9 @@ const handleSubmit = async (e) => {
         >
           Delete account
         </span>
-          <span className='text-red-700 cursor-pointer'>
-            Sign Out
-          </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
        </div>
        <p className='text-red-700 mt-5'>{error ? error : ''}</p>
        <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
